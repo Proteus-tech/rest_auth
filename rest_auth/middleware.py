d05e96cfa62b79856d59007a9e0d224aff342503
Pam Suebvisai <sinapam@proteus-tech.com>
2012-02-02 21:23:49 +0700

    add logging

--- rest_auth/middleware.py
+++ rest_auth/middleware.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-import urllib
 from datetime import datetime, timedelta
 from django.conf import settings
 from rest_auth.signature import get_signature
 
+import logging
+logger = logging.getLogger(__name__)
+
 class AjaxUserMiddleware(object):
     def process_request(self,request):
         if not request.user.username:
+            logger.info('path: %s' % request.path)
             time_format = '%Y-%m-%d %H:%M:%S'
             current_user = ''
             input_signature = ''
@@ -18,6 +21,8 @@ class AjaxUserMiddleware(object):
             if params:
                 current_user = params.get('current_user')
                 signed_value = params.get('signed_value')
+                logger.info('current_user: %s' % current_user)
+                logger.info('signed_value: %s' % signed_value)
                 if signed_value:
                     splitted_signed_value = signed_value.split('TIME')
                     if len(splitted_signed_value) == 2:
@@ -28,12 +33,15 @@ class AjaxUserMiddleware(object):
             delta = timedelta(0, getattr(settings,
                 'FOST_AUTHN_MAXIMUM_CLOCK_SKEW', 300))
             skew = max(signed_time - utc_now, utc_now - signed_time)
+            logger.info('skew: %s' % skew)
             if (skew < delta) and current_user and input_signature:
                 path = request.path
                 body = request.raw_post_data
 
                 document, test_signature = get_signature(request,request.method,request.build_absolute_uri(path),signed_time,[current_user,])
 
+                logger.info('input_signature: %s' % input_signature)
+                logger.info('test_signature: %s' % test_signature)
                 if input_signature == test_signature:
                     timestamp = datetime.utcnow().strftime(time_format)
                     # if they've passed this check, now it's time to put their user in so Fost authentication accepts
