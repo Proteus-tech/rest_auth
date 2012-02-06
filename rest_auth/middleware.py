b1bb5f55aba5fbd3c7d930b55f6eefa0fc24361a
Pam Suebvisai <sinapam@proteus-tech.com>
2012-02-06 12:39:26 +0700

    update middleware so that it doesn't re-process things that already have headers

--- rest_auth/middleware.py
+++ rest_auth/middleware.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime, timedelta
 from django.conf import settings
-from rest_auth.signature import get_signature
+from rest_auth import signature as ra_signature # import like this for mocking in tests
 
 import logging
 logger = logging.getLogger(__name__)
 
 class AjaxUserMiddleware(object):
-    def process_request(self,request):
-        if not request.user.username:
+    def authentication_headers_exist(self, request):
+        headers = ['HTTP_X_FOST_USER','HTTP_X_FOST_HEADERS','HTTP_X_FOST_TIMESTAMP','HTTP_AUTHORIZATION']
+        for header in headers:
+            if not request.META.has_key(header):
+                return False
+        return True
+
+
+    def process_request(self, request):
+        if not request.user.username and not self.authentication_headers_exist(request):
             logger.info('path: %s' % request.path)
             time_format = '%Y-%m-%d %H:%M:%S'
             current_user = ''
             input_signature = ''
             signed_time = datetime.min
-            params = request.GET
+            params = request.GET.copy()
 
             if params:
                 current_user = params.get('current_user')
@@ -41,14 +49,14 @@ class AjaxUserMiddleware(object):
                 path = request.path
                 body = request.raw_post_data
 
-                document, test_signature = get_signature(request,request.method,request.build_absolute_uri(path),signed_time,[current_user,])
+                document, test_signature = ra_signature.get_signature(request,request.method,request.build_absolute_uri(path),signed_time,[current_user,])
 
                 logger.info('input_signature: %s' % input_signature)
                 logger.info('test_signature: %s' % test_signature)
                 if input_signature == test_signature:
                     timestamp = datetime.utcnow().strftime(time_format)
                     # if they've passed this check, now it's time to put their user in so Fost authentication accepts
-                    document, signature = get_signature(request,request.method,path,timestamp,[current_user,],body)
+                    document, signature = ra_signature.get_signature(request,request.method,path,timestamp,[current_user,],body)
 
                     request.META['HTTP_X_FOST_USER'] = current_user
                     request.META['HTTP_X_FOST_HEADERS'] = 'X-FOST-User'
